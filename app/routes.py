23f4f2ffcc4ae25059050e3526351d25040e4b52
Caleb Andreasen <caleb.andreasen@gmail.com>
2024-05-15 20:09:18 +0200

    html templates + login
    Still just following the Miguel Grinberg microblog tutorial.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,28 @@
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Caleb'}
+    posts = [
+        {
+            'author': {'username': 'Alice'},
+            'body': 'Sample post 1.'
+        },
+        {
+            'author': {'username': 'Bob'},
+            'body': 'Sample post 2.'
+        }
+    ]
+
+    return render_template('index.html', title="Home", user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title="Sign In", form=form)
