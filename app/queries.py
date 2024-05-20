fecc583b2aace5355c7f1ac7c91459d5a7c94332
Leo Rønsholt Mikkelsen <leo@strandjaegervej.dk>
2024-05-20 17:27:35 +0200

    Implemented country, state, and city to registration form. Changed queries to return a list of dictionaries.

--- app/queries.py
+++ app/queries.py
@@ -2,13 +2,14 @@ from app import db, conn
 from app.models import User
 
 def get_user_by_username(username):
-    db.execute("SELECT * FROM Users WHERE username = %s;", (username,))
+    db.execute("SELECT * FROM Users WHERE username = %s;", (username, ))
     return User(db.fetchone()) if db.rowcount == 1 else None
 
 def insert_user(user):
     query = """INSERT INTO 
     Users(username, password_hash, country, state_id, city_id)
     VALUES (%s, %s, %s, %s, %s);"""
+
     db.execute(query, (user.username, user.password_hash, user.country, user.state_id, user.city_id))
     conn.commit()
 
@@ -16,25 +17,16 @@ def get_countries():
     query = "SELECT * FROM Countries"
     db.execute(query)
     res = db.fetchall()
-    countries = list()
-    for c in res:
-        countries.append(c["country_name"])
-    return countries
+    return res
 
 def get_states():
     query = "SELECT * FROM States"
     db.execute(query)
     res = db.fetchall()
-    states = list()
-    for c in res:
-        states.append((c["id"],c["state_name"]))
-    return states 
+    return res 
 
 def get_cities():
     query = "SELECT * FROM Cities"
     db.execute(query)
     res = db.fetchall()
-    cities = list()
-    for c in res:
-        cities.append((c["id"],c["city_name"]))
-    return cities
+    return res 
